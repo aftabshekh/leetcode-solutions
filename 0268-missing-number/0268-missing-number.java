dc2866655ec1490588d8073b4fb9668d9d0ed5e6
Aftab Shekh <aftabshekh8177@gmail.com>
2026-08-25 22:16:45 +0530

    Time: 0 ms (100%), Space: 46.9 MB (97.68%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,42 @@
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int len = nums.length;
+//         int []temp = new int[len+1];
+//         for(int i=0; i<len; i++){
+//             temp[nums[i]]++;
+//         }
+//         for(int i=0; i<=len; i++){
+//             if(temp[i]==0){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+
+
+
+class Solution {
+    public int missingNumber(int[] nums) {
+    int len = nums.length;
+    int []temp = new int[len+1];
+
+    for(int i= 0; i<len; i++){
+      temp[nums[i]]++;
+    }
+
+    for(int i=0; i<=len; i++){
+        if(temp[i]==0){
+            return i;
+        }
+    }
+    return -1;
+
+    }
+}
+
+
+
+
